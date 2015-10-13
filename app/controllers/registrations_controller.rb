3e8bc992c7d9d13128c3fb0166348b36aa24408a
fabienoger <fab-oger@live.fr>
2015-10-13 12:14:42 +0200

    add role to edit account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :address, :zipcode, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:role, :first_name, :last_name, :address, :zipcode, :email, :password, :password_confirmation, :current_password)
   end
 end
